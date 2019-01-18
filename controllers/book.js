711775f6b2be08036d872d6740865060e69583b4
patriagani <patria.gani@gmail.com>
2019-01-18 11:10:26 +0700

    get books done

--- controllers/book.js
+++ controllers/book.js
@@ -1,23 +1,43 @@
 const Book = require("../models/Book")
+const jwt = require('jsonwebtoken');
 
 
 module.exports = {
     add_book: (req, res) => {
+      console.log(req.file, "=============================");
+      if (req.headers.token) {
+        var decoded = jwt.verify(req.headers.token, process.env.JWT_SECRET );
         let newData = {
-            title: req.body.title,
-            description: req.body.description,
-            creator: '5c08731b9bb6850798aac569',
-            cover: "https://visual-integrity.com/wp-content/uploads/2016/02/pdf-page.png",
-            file: req.file.cloudStoragePublicUrl
+          title: req.body.title,
+          description: req.body.description,
+          creator: decoded._id,
+          cover: "https://visual-integrity.com/wp-content/uploads/2016/02/pdf-page.png",
+          file: req.file.cloudStoragePublicUrl
         }
-
         Book.create(newData)
-            .then((result) => {
-                res.status(201).json(result);
-            }).catch((err) => {
-                res.status(400).json({
-                    message: 'Internal server error'
-                })
-            });
+        .then((result) => {
+          res.status(201).json(result);
+        }).catch((err) => {
+          res.status(500).json({
+            message: 'Internal server error'
+          })
+        });
+      }
+      else {
+        res.status(401).json({
+          message: "Unautorized"
+        })
+      }
     },
+
+    get_books: (req, res) => {
+      Book.find()
+          .then((result) => {
+            res.status(201).json(result);
+          }).catch((err) => {
+            res.status(400).json({
+                message: 'Internal server error'
+            })
+          })
+    }
 }
--- routes/books.js
+++ routes/books.js
@@ -6,4 +6,6 @@ var bookController = require('../controllers/book')
 router.post('/', upload.multer.single('file'),
     upload.sendUploadToGCS, bookController.add_book)
 
+router.get('/', bookController.get_books)
+
 module.exports = router;
