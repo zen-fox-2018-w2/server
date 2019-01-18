b25c799e9e4ab3bb8d5c8c1ccd8f60fa7e673b0a
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2019-01-18 11:29:19 +0700

    solve

--- controllers/book.js
+++ controllers/book.js
@@ -4,41 +4,41 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     add_book: (req, res) => {
-      console.log(req.file, "=============================");
-      if (req.headers.token) {
-        var decoded = jwt.verify(req.headers.token, process.env.JWT_SECRET );
-        let newData = {
-          title: req.body.title,
-          description: req.body.description,
-          creator: decoded._id,
-          cover: "https://visual-integrity.com/wp-content/uploads/2016/02/pdf-page.png",
-          file: req.file.cloudStoragePublicUrl
+        console.log(req.file, "=============================");
+        if (req.headers.token) {
+            var decoded = jwt.verify(req.headers.token, process.env.JWT_SECRET);
+            let newData = {
+                title: req.body.title,
+                description: req.body.description,
+                creator: decoded._id,
+                cover: "https://visual-integrity.com/wp-content/uploads/2016/02/pdf-page.png",
+                file: req.file.cloudStoragePublicUrl
+            }
+            Book.create(newData)
+                .then((result) => {
+                    res.status(201).json(result);
+                }).catch((err) => {
+                    res.status(500).json({
+                        message: 'Internal server error'
+                    })
+                });
         }
-        Book.create(newData)
-        .then((result) => {
-          res.status(201).json(result);
-        }).catch((err) => {
-          res.status(500).json({
-            message: 'Internal server error'
-          })
-        });
-      }
-      else {
-        res.status(401).json({
-          message: "Unautorized"
-        })
-      }
+        else {
+            res.status(401).json({
+                message: "Unautorized"
+            })
+        }
     },
 
     get_books: (req, res) => {
-      Book.find()
-          .then((result) => {
-            res.status(201).json(result);
-          }).catch((err) => {
-            res.status(400).json({
-                message: 'Internal server error'
+        Book.find()
+            .then((result) => {
+                res.status(201).json(result);
+            }).catch((err) => {
+                res.status(400).json({
+                    message: 'Internal server error'
+                })
             })
-          })
     },
     delete_ownCollection: (req, res) => {
         console.log(req.params.id)
